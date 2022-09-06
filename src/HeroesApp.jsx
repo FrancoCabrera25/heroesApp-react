a2a182ea9b07c3b86ad4a8a695b090e6b55f5e6b
Franco Cabrera <franco.cabrera@naranja.com>
2022-09-05 23:05:00 -0300

    add routers

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
 export const HeroesApp = () => {
   return (
   <>
-  <button className='btn btn-primary'>Hola</button>
+  <AppRouter />
   </>
   )
 }
